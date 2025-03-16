190dcaa09dae36311bf51b675d249dd5c71aee20
Din Bećarević <din.velenje@gmail.com>
2025-03-16 02:32:12 +0100

    Add .gitignore and main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import time
+
+#.\venv\Scripts\activate
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    # TODO: return (lower_bound, upper_bound) as np.ndarray of shape (1,3)
+    pass
+
+
+def zmanjsaj_sliko(slika, sirina, visina):
+    # TODO: return resized image as np.ndarray
+    pass
+
+
+def prestej_piksle_z_barvo_koze(slika, barva_koze):
+    # TODO: return number of skin pixels in the image (square area)
+    pass
+
+
+def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze):
+    # TODO: return list of square regions where color matches skin color
+    pass
+
+
+def main():
+    # TODO: implement the main logic here
+    pass
+
+
+if __name__ == "__main__":
+    main()
